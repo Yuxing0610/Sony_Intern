22687eeb49975b217aaafb81a50379805ed8d1f9
Yuxing0610 <yxyao0610@gmail.com>
2023-05-31 16:18:16 +0200

    first demo

---
+++ docs/_build/html/_sources/pages/data-preparation.rst.txt
@@ -0,0 +1,10 @@
+=====================================
+Downloading/Updating the new datasets
+=====================================
+
+Run the following command from the root directory to download the up-to-date version of the datasets:
+
+::
+    
+    USERNAME=josifosk  # your GASPAR username
+    bash scraper/download_scrapped_datasets.sh $USERNAME
--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sony Project'
-copyright = '2023, YuxingYao'
-author = 'YuxingYao'
-release = '1.0'
+project = 'FLOWS'
+copyright = '2023'
+author = 'FLOWS Project'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
